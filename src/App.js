e37cdeff49a6f747b9267c09808bc3e86cf95e00
Ben Web <ben@benweb.com>
2018-10-27 23:26:56 +0100

    add routing and basic nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import {
   Container,
   Col,
+  Jumbotron,
   Row
 } from 'reactstrap';
+import Navigation from './components/Navigation';
+import ProductList from './components/ProductList';
+
 
 class App extends Component {
 
   render() {
 
     return (
-      <div>
-        <h1>Bad Basket</h1>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <header>
+              <Jumbotron>
+                <h1 className="display-3">Bad Basket</h1>
+                <p className="lead">Your wallet friendly store</p>
+              </Jumbotron>
+              <Navigation />
+            </header>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <main>
+              <Switch>
+                <Route exact path="/"
+                  render={({ match }) =>
+                    <ProductList />
+                  } /> />
+            </Switch>
+            </main>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <footer>
+              <p><a href="#top"><span role="img" aria-label="Back to top of page">⬆️</span></a></p>
+              <p><small>&copy; Bad Basket</small></p>
+            </footer>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <p>list of products here</p>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
